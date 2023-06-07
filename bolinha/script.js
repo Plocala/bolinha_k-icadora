aa691e73636bf8510d4ef12288f3a498dba0d0d5
Plocala <120675057+Plocala@users.noreply.github.com>
2023-06-07 11:40:25 -0300

    version 2
    com esse código eu finalizo o necessário para ele

--- bolinha/script.js
+++ bolinha/script.js
@@ -1,74 +1,94 @@
+// Get references to HTML elements
 const objectId = {
   bolinha: document.getElementById("bolinha"),
   startButton: document.getElementById("startButton"),
 
 };
 
+// Object to store animation variables
 const objectVar = {
   visibleBolinha: false,
   moveX: 1.5,
   moveY: 1.5,
-  directionX: 0,
-  directionY: 0,
+  directionX: 1,
+  directionY: 1,
 
 };
 
-objectId.startButton.addEventListener('click', function(event){
-  if(event.button == 0){
+// Event listener for the start button
+objectId.startButton.addEventListener('click', function(event) {
+  if (event.button === 0) {
     objectVar.visibleBolinha = true;
-
+    startBolinha();
   }
 
 });
 
-objectId.bolinha.addEventListener('click', function(event){
-  if(event.button == 0){
-    objectVar.directionX = Math.ceil(Math.random()*((1+Math.sqrt(5))/2));
-    objectVar.directionY = Math.ceil(Math.random()*((1+Math.sqrt(5))/2));
-    
-    if(Math.random() < 0.5){
+// Event listener for the ball
+objectId.bolinha.addEventListener('click', function(event) {
+  if (event.button === 0) {
+    objectVar.directionX = Math.round(Math.random() * ((1 + Math.sqrt(5)) / 2));
+    objectVar.directionY = Math.round(Math.random() * ((1 + Math.sqrt(5)) / 2));
+
+    if (Math.random() < 0.5) {
       objectVar.directionX = -objectVar.directionX;
       objectVar.directionY = -objectVar.directionY;
 
     }
-    
+
   }
 
 });
 
-function startBolinha(){
-  if(objectVar.visibleBolinha === true){
+// Function to start the ball animation
+function startBolinha() {
+  if (objectVar.visibleBolinha === true) {
     objectId.bolinha.style.visibility = "visible";
     objectId.startButton.style.visibility = "hidden";
-
+    update();
+  
   }
 
 }
 
-function barreira(){
-  if(objectId.bolinha.style.left > window.innerWidth || objectId.bolinha.style.left < window.innerWidth){
+// Function to handle ball collisions with window boundaries
+function barreira() {
+  const bolinhaStyle = getComputedStyle(objectId.bolinha);
+  const bolinhaLeft = parseInt(bolinhaStyle.left);
+  const bolinhaTop = parseInt(bolinhaStyle.top);
+  const bolinhaWidth = parseInt(bolinhaStyle.width);
+  const bolinhaHeight = parseInt(bolinhaStyle.height);
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  if (bolinhaLeft + bolinhaWidth > windowWidth || bolinhaLeft < 0) {
     objectVar.directionX = -objectVar.directionX;
 
   }
-  if(objectId.bolinha.style.top > window.innerHeight || objectId.bolinha.style.top > window.innerHeight){
+
+  if (bolinhaTop + bolinhaHeight > windowHeight || bolinhaTop < 0) {
     objectVar.directionY = -objectVar.directionY;
 
   }
 
 }
 
-function update(){
-
+// Function to update the ball position and handle collisions
+function update() {
   barreira();
 
-  objectVar.directionX = objectVar.moveX;
-  objectVar.directionY = objectVar.moveY;
+  const bolinhaStyle = getComputedStyle(objectId.bolinha);
+  const bolinhaLeft = parseInt(bolinhaStyle.left);
+  const bolinhaTop = parseInt(bolinhaStyle.top);
+  const velocity = 1.618;
 
-  objectId.bolinha.style.left = `${objectVar.directionX}px`;
-  objectId.bolinha.style.top = `${objectVar.directionY}px`;
+  objectId.bolinha.style.transform = `rotate(${objectVar.directionX * bolinhaLeft}deg)`;
+  objectId.bolinha.style.left = `${bolinhaLeft + objectVar.directionX * objectVar.moveX * velocity}px`;
+  objectId.bolinha.style.top = `${bolinhaTop + objectVar.directionY * objectVar.moveY * velocity}px`;
 
   requestAnimationFrame(update);
 
 }
 
-requestAnimationFrame(update);
+// Start the animation
+startBolinha();
